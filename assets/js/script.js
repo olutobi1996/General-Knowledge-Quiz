5503ab123f9c987b09f0655f6ba5bda772f40148
Olutobi Adebayo- Rowling <olutobi1996@icloud.com>
2024-08-08 19:50:41 +0000

    created whole new respository as work got deleted

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+const questions = [
+    {
+        question: "what football team won the world cup in 1966?", 
+        answers: [
+            { text: "England", correct: true},
+            { text: "Germany", correct: false},
+            { text: "Brazil", correct: false},
+            { text: "France", correct: false},
+        ] 
+    },
+    {
+        question: "How many coloured rings in the Olympic Flag?", 
+        answers: [
+            { text: "2", correct: false},
+            { text: "6", correct: false},
+            { text: "4", correct: false},
+            { text: "5", correct: true},
+        ]
+    },
+    {
+        question: "Who directed the film pulp fiction?", 
+        answers: [
+            { text: "Daniel Day Lewis", correct: false},
+            { text: "Quentin Taratino", correct: true},
+            { text: "Steven Speilberg", correct: false},
+            { text: "Martin Scorses", correct: false}, 
+        ]
+    },
+    {
+        question: "In the film Lion King Mufasa dies from being trampled on by a pack of?", 
+        answers: [
+            { text: "sheep", correct: false},
+            { text: "hyenas", correct: false},
+            { text: "wildebeests", correct: true},
+            { text: "bears", correct: false},
+        ]
+    },
+    {
+        question: "Who wrote the novel of mice and men?", 
+        answers: [
+            { text: "Virginia Woolf", correct: false},
+            { text: "Elaine Anderson", correct: false},
+            { text: "William Faulkner", correct: false},
+            { text: "John Steinbeck", correct: true},
+        ]
+    },
+    {
+        question: "What is the name of the fourth book in the Harry Potter Series?", 
+        answers: [
+            { text: "Harry Potter and the Goblet of Fire", correct: true},
+            { text: "Harry Potter and the Deathly Hallows", correct: false},
+            { text: "Harry Potter and Philosophers Stone", correct: false},
+            { text: "Harry Potter and Half Blood Prince", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const nextButton = document.getElementById("next");
+const resultElement = document.getElementById('result'); 
+ 
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "next";
+    nextButton.addEventListener("click", nextQuestion)
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        document.getElementById("answer-buttons").appendChild(button);
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    }); 
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    document.getElementById("answer-buttons").innerHTML = ""
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        score++
+        document.getElementById("score").innerHTML = "Score: " + score + "/6"
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(document.getElementsByClassName("btn")).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    });
+    nextButton.style.display = "block";
+}
+
+function nextQuestion() {
+    currentQuestionIndex++
+    if (currentQuestionIndex > 5) {
+        startQuiz()
+        return
+    }
+    showQuestion()
+}
+
+
+startQuiz();
